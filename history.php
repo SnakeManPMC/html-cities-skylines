f24b1b7631ff682203b49f5efd8714d79121db4d
Snake Man <snakeman@phnet.fi>
2024-10-22 11:51:10 +0000

    history updated, well added pretty much

--- history.php
+++ history.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>History Cities Skylines - PMC Tactical</title>
+<title>History of Releases Cities Skylines - PMC Tactical</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="History Cities Skylines - PMC Tactical">
+<META name="description" content="History of Releases Cities Skylines - PMC Tactical">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,16 +15,81 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1>History Cities Skylines - PMC Tactical</h1>
+	<h1>History of Releases Cities Skylines - PMC Tactical</h1>
+
+<p>
+Important announcements and releases history for Cities Skylines games.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
-	<h2>History</h2>
+<?php include("/var/www/include/support.php"); ?>
+	<h2>History of Releases</h2>
+
+<p>
+<a href="cs1/index.php">Cities Skylines 1 (CS1)</a> and older games history is completely missing, sorry. Below you find detailed UTC timestamps for CS2 releases.
+</p>
 
 <p>
 2023-10-24 Cities Skylines 2 released.
+</p>
+
+<p>
+2023-10-26T14:20:00Z v1.0.11f1 hotfix <a href="https://store.steampowered.com/news/app/949230/view/3709336747339206365" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2023-11-02T15:35:00Z v1.0.12.F1 hotfix <a href="https://store.steampowered.com/news/app/949230/view/3811794273437602081" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2023-11-09T15:01:00Z v1.0.13f1 hotfix. <a href="https://store.steampowered.com/news/app/949230/view/3825305710230606671" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2023-11-16T15:00:00Z v1.0.14f1 hotfix. <a href="https://store.steampowered.com/news/app/949230/view/3845572543558454885" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2023-12-01T13:00:00Z v1.0.15f1 hotfix. <a href="https://store.steampowered.com/news/app/949230/view/3865840010278875953" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2023-12-14T13:00:00Z v1.0.18f1 hotfix <a href="https://store.steampowered.com/news/app/949230/view/6024191483879077826" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2024-01-31T13:00:00Z v1.0.19f1 hotfix <a href="https://store.steampowered.com/news/app/949230/view/4060626400889852437" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2024-03-25T17:00:17Z v1.1.0f1 Modding Wavelet <a href="https://store.steampowered.com/news/app/949230/view/4116925831592681669" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2024-10-22T11:33:00Z v1.1.1f1 hotfix <a href="https://store.steampowered.com/news/app/949230/view/4179976861331917580" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2024-04-25T08:00:00Z v1.1.2f1 <a href="https://store.steampowered.com/news/app/949230/view/4188986596614839029" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2024-06-24T08:00:00Z v1.1.5f1 <a href="https://store.steampowered.com/news/app/949230/view/4188991035537036941" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2024-07-03T08:07:00Z Detailer's Patch #1 v1.1.6f1 <a href="https://store.steampowered.com/news/app/949230/view/4244161399611889013" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2024-07-04T13:54:00Z v1.1.7f1 hotfix <a href="https://store.steampowered.com/news/app/949230/view/4244161399621773161" target="_blank">Patch Notes</a>.
+</p>
+
+<p>
+2024-09-04T08:00:00Z v1.1.8f1 - Decorations Patch <a href="https://store.steampowered.com/news/app/949230/view/4620217676525954573" target="_blank">Patch Notes</a>.
 </p>
 
 <?php include("/var/www/include/section-end.php"); ?>
@@ -33,7 +98,6 @@
 <footer>
 <?php include("/var/www/include/footer-start.php"); ?>
 <p>Back to <a href="index.php">PMC Cities Skylines root page</a></p>
-<br><br>
 <p><i>PMC Cities Skylines 2022 - <?php print(date("Y")); ?>.</i></p>
 <?php include("/var/www/include/footer-end.php"); ?>
 <?php include("/var/www/include/support.php"); ?>
